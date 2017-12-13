ec27abacdfcb0cc0e69b116662c73bcb2793f4b2
Robert <github@vllmrt.net>
2017-12-13 16:45:02 +0100

    nicer paragraphing for the index page (#7)
    * nicer paragraphing for the index
    
    * less br
    
    * less indentation
    
    * balanced paragraphs

--- web/index.go
+++ web/index.go
@@ -60,26 +60,24 @@ var (
 	indexTempl = withBase(`
 {{define "page"}}
 <h2>What</h2>
-<div><p>
-Verssion tracks stable version of software projects (e.g.: databases, editors, JS frameworks), and makes that available as an RSS (atom) feed. The main use-case is for dev-ops and developers who use a lot of open source software projects, and who like to keep an eye on releases. Without making that a fulltime job, and without signing up for dozens of e-mail lists. Turns out wikipedia is a great source for version information, so that's what we use.<br />
-You can create feeds for your own use, or share them with colleagues.<br />
-</p>
-<br />
-<a href="https://github.com/alicebob/verssion/">Full source</a> for issues and suggestions.<br />
-</div>
-<br />
+<p>Verssion tracks stable version of software projects (e.g.: databases, editors, JS frameworks), and makes that available as an RSS (atom) feed. The main use-case is for dev-ops and developers who use a lot of open source software projects, and who like to keep an eye on releases. Without making that a fulltime job, and without signing up for dozens of e-mail lists. Turns out wikipedia is a great source for version information, so that's what we use.</p>
+<p>You can create feeds for your own use, or share them with colleagues.</p>
+<p><a href="https://github.com/alicebob/verssion/">Full source</a> for issues and suggestions.</p>
 
 <h2>Feed</h2>
-Make a feed which combines multiple projects in a single feed:<br />
-<a href="./curated/">Create new feed!</a><br />
-	{{- if .curated}}
-		<br />
-		Your recent feeds:<br />
-		{{- range $id, $cur := .curated}}
-			- <a href="{{$.base}}/curated/{{$id}}/">{{$cur.Title}}</a><br />
-		{{- end}}
+<p>Make a feed which combines multiple projects in a single feed:
+<a href="./curated/">Create new feed!</a></p>
+
+{{- if .curated}}
+	<p>
+	Your recent feeds:
+	<ul>
+	{{- range $id, $cur := .curated}}
+		<li><a href="{{$.base}}/curated/{{$id}}/">{{$cur.Title}}</a></li>
 	{{- end}}
-<br />
+	</ul>
+	</p>
+{{- end}}
 
 <h2>Updates</h2>
 	<table>
@@ -94,7 +92,6 @@ Make a feed which combines multiple projects in a single feed:<br />
 			<td></td>
 		</tr>
 	</table>
-	<br />
 
 {{- end}}
 `)
